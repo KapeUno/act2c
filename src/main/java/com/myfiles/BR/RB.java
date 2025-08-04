932068e0eaf3a039b2bf160beaaf808f3f783786
CL3-PC19 <CL3-PC19@DESKTOP-E9G0JN5>
2025-08-04 14:08:13 +0800

    SAMPLE PROGRAM ACT2C

---
+++ src/main/java/com/myfiles/BR/RB.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myfiles.BR;
+
+/**
+ *
+ * @author CL3-PC16
+ */
+public class RB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Kaya, Paba?");
+    }
+}
